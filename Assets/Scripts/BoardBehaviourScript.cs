99cac22056e884712f1a612c707720239ffd914b
Kim Ju Hyeon <saykjh94@naver.com>
2017-07-07 13:09:53 +0900

    create base
    can make deck
    can draw card
    can place card

---
+++ Assets/Scripts/BoardBehaviourScript.cs
@@ -0,0 +1,232 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardBehaviourScript : MonoBehaviour {
+
+    public static BoardBehaviourScript instance;
+    public Transform MyDeckPos;
+    public Transform MyHandPos;
+    public Transform MyTablePos;
+    public Transform AIDeckPos;
+    public Transform AIHandPos;
+    public Transform AITablePos;
+
+    public List<GameObject> MyDeckCards = new List<GameObject>();
+    public List<GameObject> MyHandCards = new List<GameObject>();
+    public List<GameObject> MyTableCards = new List<GameObject>();
+    public List<GameObject> AIDeckCards = new List<GameObject>();
+    public List<GameObject> AIHandCards = new List<GameObject>();
+    public List<GameObject> AITableCards = new List<GameObject>();
+
+    public enum Turn { MyTurn, AITurn };
+    public Turn turn = Turn.MyTurn;
+
+    int maxMyMana = 1;
+    int maxAIMana = 1;
+    int MyMana = 1;
+    int AIMana = 1;
+
+    public TextMesh MyManaText;
+    public TextMesh AIManaText;
+
+    public bool gameStarted = false;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+        foreach (GameObject CardObject in GameObject.FindGameObjectsWithTag("Card"))
+        {
+            CardObject.GetComponent<Rigidbody>().isKinematic = true;
+            CardBehaviourScript c = CardObject.GetComponent<CardBehaviourScript>();
+
+            // 서로의 덱의 카드를 찾아 리스트에 넣는다
+            if (c.team == CardBehaviourScript.Team.My)
+            {
+                MyDeckCards.Add(CardObject);
+                c.GetComponent<CardBehaviourScript>().newPos = MyDeckPos.position;
+            }
+            else
+            {
+                AIDeckCards.Add(CardObject);
+                c.GetComponent<CardBehaviourScript>().newPos = AIDeckPos.position;
+            }
+        }
+
+        //Start Game
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        gameStarted = true;
+        UpdateGame();
+
+        // 맨 처음 3장씩 뽑는다
+        for (int i = 0; i < 3; i++)
+        {
+            DrawCardFromDeck(CardBehaviourScript.Team.My);
+            DrawCardFromDeck(CardBehaviourScript.Team.AI);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    void UpdateGame()
+    {
+        MyManaText.text = MyMana.ToString() + "/" + maxMyMana;
+        AIManaText.text = AIMana.ToString() + "/" + maxAIMana;
+
+        /*
+        if (MyHero.health <= 0)
+            EndGame(AIHero);
+        if (AIHero.health <= 0)
+            EndGame(MyHero);
+            */
+
+        //UpdateBoard();
+    }
+    /*
+    public void EndGame(HeroBehaviourScript winner)
+    {
+        if (winner == MyHero)
+        {
+            Debug.Log("MyHero");
+            Time.timeScale = 0;
+            winnertext.text = "You Won";
+            //Destroy(this);
+        }
+
+        if (winner == AIHero)
+        {
+            Time.timeScale = 0;
+            Debug.Log("AIHero");
+            winnertext.text = "You Losse";
+            //Destroy(this);
+        }
+    }*/
+
+    public void DrawCardFromDeck(CardBehaviourScript.Team team)
+    {
+        if (team == CardBehaviourScript.Team.My && MyDeckCards.Count != 0 && MyHandCards.Count <= 7)
+        {
+            int random = Random.Range(0, MyDeckCards.Count);
+            GameObject tempCard = MyDeckCards[random];
+
+            // 카드를 패로 옮긴다
+            //tempCard.transform.position = MyHandPos.position;
+            tempCard.GetComponent<CardBehaviourScript>().newPos = MyHandPos.position;
+            tempCard.GetComponent<CardBehaviourScript>().SetCardStatus(CardBehaviourScript.CardStatus.InHand);
+
+            // 카드를 뽑았으면 덱 리스트에서 제거하고 패 리스트에 추가한다
+            MyDeckCards.Remove(tempCard);
+            MyHandCards.Add(tempCard);
+        }
+
+        if (team == CardBehaviourScript.Team.AI && AIDeckCards.Count != 0 && AIHandCards.Count < 7)
+        {
+            int random = Random.Range(0, AIDeckCards.Count);
+            GameObject tempCard = AIDeckCards[random];
+
+            tempCard.transform.position = AIHandPos.position;
+            tempCard.GetComponent<CardBehaviourScript>().SetCardStatus(CardBehaviourScript.CardStatus.InHand);
+
+            AIDeckCards.Remove(tempCard);
+            AIHandCards.Add(tempCard);
+        }
+        HandPositionUpdate();
+    }
+
+    public void HandPositionUpdate()
+    {
+        float space = 0f;
+        float space2 = 0f;
+        float gap = 1.8f;
+
+        foreach (GameObject card in MyHandCards)
+        {
+            int numberOfCards = MyHandCards.Count;
+            card.GetComponent<CardBehaviourScript>().newPos = MyHandPos.position + new Vector3(-(numberOfCards - 1) * 0.9f + space, 0, 0);
+            space += gap;
+        }
+
+        foreach (GameObject card in AIHandCards)
+        {
+            int numberOfCards = AIHandCards.Count;
+            card.GetComponent<CardBehaviourScript>().newPos = AIHandPos.position + new Vector3(-(numberOfCards - 1) * 0.9f + space2, 0, 0);
+            space2 += gap;
+        }
+    }
+
+    public void TablePositionUpdate()
+    {
+        float space = 0f;
+        float space2 = 0f;
+        float gap = 1.8f;
+
+        foreach (GameObject card in MyTableCards)
+        {
+            int numberOfCards = MyTableCards.Count;
+            //card.transform.position = myTablePos.position + new Vector3(-numberOfCards + space - 2,0,0);
+            card.GetComponent<CardBehaviourScript>().newPos = MyTablePos.position + new Vector3(-(numberOfCards - 1) * 0.9f + space, 0, 0);
+            space += gap;
+        }
+
+        foreach (GameObject card in AITableCards)
+        {
+            int numberOfCards = AITableCards.Count;
+            //card.transform.position = AITablePos.position + new Vector3(-numberOfCards + space2,0,0);
+            card.GetComponent<CardBehaviourScript>().newPos = AITablePos.position + new Vector3(-(numberOfCards - 1) * 0.9f + space2, 0, 0);
+            space2 += gap;
+        }
+    }
+
+
+    public void PlaceCard(CardBehaviourScript card)
+    {
+        Debug.Log("place card 2");
+        if (card.team == CardBehaviourScript.Team.My && MyMana - card.mana >= 0 && MyTableCards.Count < 5)
+        {
+            card.GetComponent<CardBehaviourScript>().newPos = MyTablePos.position; // 카드의 새 위치를 테이블로 지정
+
+            // 카드를 냈으면 패 리스트에서 제거하고 테이블 리스트에 추가한다
+            MyHandCards.Remove(card.gameObject);
+            MyTableCards.Add(card.gameObject);
+            card.SetCardStatus(CardBehaviourScript.CardStatus.OnTable);
+
+            MyMana -= card.mana;
+        }
+
+        if(card.team == CardBehaviourScript.Team.AI && AIMana - card.mana >= 0 && AITableCards.Count < 5)
+        {
+            card.GetComponent<CardBehaviourScript>().newPos = MyTablePos.position; // 카드의 새 위치를 테이블로 지정
+
+            AIHandCards.Remove(card.gameObject);
+            AITableCards.Add(card.gameObject);
+
+            card.SetCardStatus(CardBehaviourScript.CardStatus.OnTable);
+
+            AIMana -= card.mana;
+        }
+        TablePositionUpdate();
+        HandPositionUpdate();
+        UpdateGame();
+    }
+
+    void OnTriggerEnter(Collider Obj)
+    {
+        CardBehaviourScript card = Obj.GetComponent<CardBehaviourScript>();
+        if (card.GetSelected() == false)
+        { 
+            card.PlaceCard();
+        }
+    }
+    
+}
---
+++ Assets/Scripts/CardBehaviourScript.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardBehaviourScript : MonoBehaviour {
+
+    public string _name = "";
+    public override string ToString()
+    {
+        return _name;
+    }
+    public Texture2D image;
+    public string description = "description";
+    public int mana;
+    public enum CardStatus { InDeck, InHand, OnTable, Destroyed};
+    public CardStatus cardStatus = CardStatus.InDeck;
+    public enum Team { My, AI };
+    public Team team = Team.My;
+
+    public TextMesh nameText;
+    public TextMesh manaText;
+    public TextMesh DescriptionText;
+
+    public Vector3 newPos;
+    float distance_to_screen;
+    bool selected = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z - 1;
+        DescriptionText.text = description.ToString();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (!selected)
+        {
+            // move position to newPos when mouse up
+            //transform.position = newPos;
+            transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * 3);
+            if (cardStatus == CardStatus.InDeck)
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0.0f, 180.0f, 0.0f), Time.deltaTime * 3);
+            }
+        }
+        //Update Visuals
+        nameText.text = _name.ToString();
+        manaText.text = mana.ToString();
+    }
+
+    public void PlaceCard()
+    {
+        if (BoardBehaviourScript.instance.turn == BoardBehaviourScript.Turn.MyTurn && cardStatus == CardStatus.InHand && team == Team.My)
+        {
+            //selected = false;
+            BoardBehaviourScript.instance.PlaceCard(this);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("On Mouse Down Event");
+        if (cardStatus == CardStatus.InHand && team == Team.My)
+        {
+            selected = true;
+        }
+
+    }
+    
+    void OnMouseUp()
+    {
+        Debug.Log("On Mouse Up Event");
+        selected = false;
+        
+    }
+    /*
+    void OnMouseOver()
+    {
+
+        Debug.Log("On Mouse Over Event");
+    }
+
+    void OnMouseEnter()
+    {
+        Debug.Log("On Mouse Enter Event");
+        //newPos += new Vector3(0,0.5f,0);
+    }
+
+    void OnMouseExit()
+    {
+        Debug.Log("On Mouse Exit Event");
+        //newPos -= new Vector3(0,0.5f, 0);
+    }
+    */
+    void OnMouseDrag()
+    {
+        //Debug.Log("On Mouse Drag Event");
+        if ( selected == true )
+        {
+            GetComponent<Rigidbody>().MovePosition(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen)));
+        }
+    }
+
+    public void SetCardStatus(CardStatus status)
+    {
+        cardStatus = status;
+    }
+
+    public bool GetSelected()
+    {
+        return selected;
+    }
+    
+}
